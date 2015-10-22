1847fb9d4da01b18919e04b7adbe4d3e04ac8697
AlexanderMann <mannimal09@gmail.com>
2015-10-21 23:26:53 -0400

    more work

--- modules/base.js
+++ modules/base.js
@@ -1,5 +1,9 @@
-games_in_season = 82; //https://en.m.wikipedia.org/wiki/National_Basketball_Association#Regular_season
-players_allowed_on_team = 15;
+var games_in_season = 82; //https://en.m.wikipedia.org/wiki/National_Basketball_Association#Regular_season
+var players_allowed_on_team = 15;
+
+var deep_copy = function(obj) {
+	return JSON.parse(JSON.stringify(obj));
+}
 
 class Score {
 	constructor(threePointersMade, assists, fieldGoalPercentage, freeThrowPercentage, points, steals, turnOvers, totalRebounds) {
@@ -21,14 +25,14 @@ class Score {
     */
 	static add(s1, s2) {
 		return new Score(
-			s1.threePointersMade + s2.threePointersMade,
-			s1.assists + s2.assists,
-			s1.fieldGoalPercentage + s2.fieldGoalPercentage,
-			s1.freeThrowPercentage + s2.freeThrowPercentage,
-			s1.points + s2.points,
-			s1.steals + s2.steals,
-			s1.turnOvers + s2.turnOvers,
-			s1.totalRebounds + s2.totalRebounds
+			s1.score_map.threePointersMade + s2.score_map.threePointersMade,
+			s1.score_map.assists + s2.score_map.assists,
+			s1.score_map.fieldGoalPercentage + s2.score_map.fieldGoalPercentage,
+			s1.score_map.freeThrowPercentage + s2.score_map.freeThrowPercentage,
+			s1.score_map.points + s2.score_map.points,
+			s1.score_map.steals + s2.score_map.steals,
+			s1.score_map.turnOvers + s2.score_map.turnOvers,
+			s1.score_map.totalRebounds + s2.score_map.totalRebounds
 			);
 	}
 
@@ -56,23 +60,44 @@ class Score {
 	}
 }
 
+var prediction = "prediction";
+var assigned = "assigned";
+
+class Draft {
+	constructor(level, team_identifier) {
+		this.level = level;
+		this.team_identifier = team_identifier;
+		this.drafted_at = new Date().getTime();
+	}
+
+	is_prediction() {
+		return this.level != assigned;
+	}
+}
+
 class Player {
 	constructor(identifier, games_expected_to_play, score) {
 		this.identifier = identifier;
 		this.games_expected_to_play = games_expected_to_play;
 		this.score = score;
-		this.drafted_at = null;
+		this.team_identifier = null;
 	}
 
 	can_draft() {
-		return this.drafted_at !== null;
+		return this.draft !== null;
 	}
 
-	draft() {
+	draft(level, team_identifier) {
 		if (this.can_draft() == false) {
 			throw "player already drafted: " + this.identifier;
 		}
-		this.drafted_at = new Date().getTime();
+		this.team_identifier = team_identifier;
+	}
+
+	reset_prediction_draft() {
+		if (this.draft.is_prediction()) {
+			this.draft = null;
+		}
 	}
 
 	score(r) {
@@ -83,6 +108,26 @@ class Player {
 			return this.score;
 		}
 		return Score.blank_score();
+	}
+}
+
+class PlayerPool {
+	constructor() {
+		this.players = {};
+	}
+
+	add_player(player) {
+		this.players[player.identifier] = player;
+	}
+
+	get_player(player_identifier) {
+		return this.players[player_identifier];
+	}
+
+	reset_prediction_drafts_for_pool() {
+		for (var player in this.players) {
+			this.players[player].reset_prediction_draft()
+		}
 	}
 }
 
@@ -97,15 +142,21 @@ class Team {
 		return Object.keys(this.players).length < players_allowed_on_team;
 	}
 
-	add_player(player) {
+	add_player(level, player) {
 		if (player.identifier in this.players) {
 			throw "identifier already used: " + player.identifier " for team: " + this.identifier;
 		}
 		if (this.can_add_player() == false) {
 			throw "max number of players has alredy been met for team: " + this.identifier;
 		}
-		player.draft();
+		player.draft(level, this.identifier);
 		this.players[player.identifier] = player;
+	}
+
+	reset_prediction_players() {
+		for (var player in this.players) {
+			delete this.players[player];
+		}
 	}
 
 	score(r) {
@@ -141,10 +192,82 @@ class Team {
 	}
 }
 
+var _order_teams = function(teams) {
+	res = [];
+	for (var k in teams) {
+		res.push(teams[k]);
+	}
+	return res;
+}
+
+var _n_random_numbers = function(n) {
+	res = [];
+	for (var i = 0; i < n; i++) {
+		res.push(Math.random());
+	}
+	return res;
+}
+
 class League {
 	constructor() {
-		teams = {};
+		this.teams = {};
+		this.player_pool = new PlayerPool();
 	}
 
-	pick_player_for_team(team_identifier, )
+	add_team(team) {
+		this.teams[team.identifier] = team;
+	}
+
+	add_player_to_team(player_identifier, team_identifier, __level=assigned) {
+		this.teams[team_identifier].add_player(
+			__level,
+			this.player_pool.get_player(player_identifier));
+	}
+
+	next_pick_for_team(team_identifier) {
+		other_teams = [];
+		for (var tid in this.teams) {
+			if (tid != team_identifier) {
+				other_teams.push(this.teams[tid]);
+			}
+		}
+		
+		top_wins = this.teams[team_identifier].wins(other_teams, _n_random_numbers(10));
+
+
+	}
+
+	all_teams_drafted() {
+		res = true;
+		for (var t in this.teams) {
+			if (this.teams[t].can_add_player()) {
+				need_to_draft = false;
+				break;
+			}
+		}
+		return res;
+	}
+
+	predict_draft_teams() {
+		if (this.all_teams_drafted()) {
+			return null;
+		}
+
+		ordered_teams = _order_teams(this.teams);
+
+		for (var team_identifier in ordered_teams) {
+			player_identifier = this.next_pick_for_team(team_identifier);
+			this.add_player_to_team(player_identifier, team_identifier, prediction);
+		}
+
+		this.predict_draft_teams();
+	}
 }
+
+
+
+
+
+
+
+
